6a267ec5942bc2ece2570d052b670203ec264e3f
Sergey Reymerov <sergeyreym@gmail.com>
2016-04-15 19:16:03 +0800

    Добавлена пустая FriendsListActivity и теперь access_token передается в неё, когда пользователь заканчивает процесс авторизации.

--- app/src/main/java/myitschool/ru/vkclient/MainActivity.java
+++ app/src/main/java/myitschool/ru/vkclient/MainActivity.java
@@ -1,8 +1,13 @@
 package myitschool.ru.vkclient;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
     private WebView mWebView;
@@ -21,5 +26,17 @@ public class MainActivity extends AppCompatActivity {
                 "&scope=friends,messages,offline" +
                 "&response_type=token" +
                 "&v=5.50");
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                Matcher m = Pattern.compile("access_token=([a-f0-9]+)").matcher(url);
+                if (m.find()) {
+                    Intent intent = new Intent(MainActivity.this, FriendsListActivity.class);
+                    intent.putExtra("access_token", m.group(1));
+                    MainActivity.this.startActivity(intent);
+                }
+            }
+        });
     }
 }
