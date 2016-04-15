5323c30625bfc6733b7957b9c2c36d04a0c7c9ec
Sergey Reymerov <sergeyreym@gmail.com>
2016-04-15 19:11:15 +0800

    Добавлен WebView на MainActivity; на нём загружается страница авторизации ВКонтакте

--- app/src/main/java/myitschool/ru/vkclient/MainActivity.java
+++ app/src/main/java/myitschool/ru/vkclient/MainActivity.java
@@ -2,12 +2,24 @@ package myitschool.ru.vkclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWebView = (WebView)findViewById(R.id.webView);
+        mWebView.loadUrl(
+                "https://oauth.vk.com/authorize" +
+                "?client_id=5410808" +
+                "&redirect_uri=https://oauth.vk.com/blank.html" +
+                "&display=mobile" +
+                "&scope=friends,messages,offline" +
+                "&response_type=token" +
+                "&v=5.50");
     }
 }
