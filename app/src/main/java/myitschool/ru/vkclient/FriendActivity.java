6f3f2e19dfc94d8da32b548d1ed7758f7c2f8b74
Sergey Reymerov <sergeyreym@gmail.com>
2016-04-16 12:41:25 +0800

    В момент запуска активности запрашиваются сведения о текущем пользователе через метод getUser(methods/users.get) и имя устанавливается в TextView на активности FriendActivity.

--- app/src/main/java/myitschool/ru/vkclient/FriendActivity.java
+++ app/src/main/java/myitschool/ru/vkclient/FriendActivity.java
@@ -2,12 +2,50 @@ package myitschool.ru.vkclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import myitschool.ru.vkclient.service.VK;
+import myitschool.ru.vkclient.service.gson.ResponseWrapper;
+import myitschool.ru.vkclient.service.gson.UserItem;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.util.List;
 
 public class FriendActivity extends AppCompatActivity {
+    VK service;
+    TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend);
+
+        mTextView = (TextView)findViewById(R.id.textView);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.vk.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        service = retrofit.create(VK.class);
+
+        service.getUser(getIntent().getStringExtra("access_token"), getIntent().getIntExtra("user_id", 0))
+                .enqueue(new Callback<ResponseWrapper<List<UserItem>>>() {
+                    @Override
+                    public void onResponse(Call<ResponseWrapper<List<UserItem>>> call, Response<ResponseWrapper<List<UserItem>>> response) {
+                        UserItem user = response.body().getResponse().get(0);
+                        mTextView.setText(user.getFirstName() + " " + user.getLastName());
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponseWrapper<List<UserItem>>> call, Throwable t) {
+                        t.printStackTrace(System.err);
+                    }
+                });
+
     }
 }
